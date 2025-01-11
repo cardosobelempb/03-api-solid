632244d0ec6b8b003e3a5055a882ff0bd43cd921
claudiobelempb <claudio.c.lima@hoymail.com>
2025-01-11 13:45:47 -0300

    Craão de um usurio

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,21 @@
 import fastify from 'fastify'
+import { z } from 'zod'
+import { prisma } from './core/lib/prisma'
 
 export const app = fastify()
+
+app.post('/users', async (request, reply) => {
+  const userCreateSchema = z.object({
+    name: z.string(),
+    email: z.string().email(),
+    password: z.string().min(6),
+  })
+
+  const { name, email, password } = userCreateSchema.parse(request.body)
+
+  await prisma.user.create({
+    data: { name, email, password_hash: password },
+  })
+
+  return reply.status(201).send()
+})
