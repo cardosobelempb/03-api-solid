0ca6fbd4482f0bceb16dbc2f4674bdbb1f8f4979
Cláudio Cardoso <claudio.c.lima@hotmail.com>
2025-01-22 18:26:36 -0300

    Validando horário do check-in

--- src/modules/check-in/application/services/check-in-validate/check-in-validate.service.ts
+++ src/modules/check-in/application/services/check-in-validate/check-in-validate.service.ts
@@ -1,7 +1,10 @@
+import { LateCheckInValidationError } from '@/core/application/errors/late-check-in-validation.error'
+import { ResourceNotFoundError } from '@/core/application/errors/resource-not-found.erro'
 import { CheckInRepository } from '@/modules/check-in/domain/repositories/check-in-repository.abstract'
+import dayjs from 'dayjs'
+
 import { CheckInRequest } from '../../request/check-in.request'
 import { CheckInResponse } from '../../response/check-in.response'
-import { ResourceNotFoundError } from '@/core/application/errors/resource-not-found.erro'
 
 export class CheckInValidateService {
   constructor(private readonly checkInRepository: CheckInRepository) {}
@@ -13,6 +16,15 @@ export class CheckInValidateService {
 
     if (!checkIn) {
       throw new ResourceNotFoundError()
+    }
+
+    const distanceInMinutesFromCheckInCreation = dayjs(new Date()).diff(
+      checkIn.created_at,
+      'minutes',
+    )
+
+    if (distanceInMinutesFromCheckInCreation > 20) {
+      throw new LateCheckInValidationError()
     }
 
     checkIn.validated_at = new Date()
